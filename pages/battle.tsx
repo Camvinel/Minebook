7b90b318cdd597e7d80c2609a82fc7d7b9ce423a
Bjiornulf <tpeev01@gmail.com>
2022-06-01 14:07:31 +0200

    [FIX] fetching users

--- pages/battle.tsx
+++ pages/battle.tsx
@@ -1,4 +1,5 @@
 import { NextPage } from "next";
+import React, { useState } from "react";
 import BattlePage from "../components/battlePage";
 import firebaseConfig from "../firebase.mjs";
 import {
@@ -13,21 +14,25 @@ import { initializeApp } from "firebase/app";
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-const getUsers = async () => {
-    const out = [];
-    const q = query(collection(db, "users"));
-    const querySnapshot = await getDocs(q);
-    querySnapshot.forEach((doc) => {
-        // doc.data() is never undefined for query doc snapshots
-        out.push(doc.data());
-    });
-    return out;
-};
 
 const Battle: NextPage = () => {
-    const users = getUsers();
+    const [users, setUsers] = useState([])
+
+    const getUsers = async () => {
+        // const out = [];
+        const q = query(collection(db, "users"));
+        const querySnapshot = await getDocs(q);
+        querySnapshot.forEach((doc) => {
+            // doc.data() is never undefined for query doc snapshots
+            // out.push(doc.data());
+            setUsers((prev) => [...prev, doc]) // add the new document to the "users" array
+        });
+        // return out;
+    };
+    // const users = getUsers();
+    getUsers();
     console.log("users", users);
-    return users.length != 0 ? (
+    return users.length >= 2 ? (
         <BattlePage user1={users[0]} user2={users[1]} />
     ) : (
         <div>Loading...</div>
